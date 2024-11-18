7f608a562af3154c9441380d59840f01a83bcba7
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-11-18 13:44:12 +0100

    feat: 10 emails print on the doc

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,7 @@
 const listElem = document.querySelector(".email-list");
 
+for(let i = 0; i < 9; i++) {
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(resp => {
+        listElem.innerHTML += `<li>${resp.data.response}</li>`
+    })
+}
