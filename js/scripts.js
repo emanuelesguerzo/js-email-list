a405356dff624153093a09086d3a26e8787b6010
emanuelesguerzo <155169753+emanuelesguerzo@users.noreply.github.com>
2024-11-18 13:45:40 +0100

    fix: minor fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 const listElem = document.querySelector(".email-list");
 
-for(let i = 0; i < 9; i++) {
+for(let i = 0; i < 10; i++) {
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(resp => {
         listElem.innerHTML += `<li>${resp.data.response}</li>`
     })
